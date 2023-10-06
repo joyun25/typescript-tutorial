d5ab4f6a3e5f0691ba2556423af5b7bb7b893aea
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-06 14:35:21 +0800

    learn Public, Private, and Readonly

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 // classes
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string; // "readonly" property means we can only read outside or inside the class itself 
+    // private details: string; // "private" property means we can access it from outside
+    // public amount: number; // default value is "public", we don't need to write it specifically
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
 
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`
@@ -19,16 +25,14 @@ const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 let invoices: Invoice[] = [];
-// invoices.push('hello') // error
-// invoices.push({name: 'shaun'}) // error
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = "yoshi";
-invTwo.amount = 400;
-// invTwo.amount = 'hello'; // error
-
-console.log(invoices);
+invoices.forEach(inv => {
+    // inv.client = 'something else'; // error
+    console.log(inv.client, inv.amount, inv.format())
+    // however, we can access the "format" because it access "details" in the "class"
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
