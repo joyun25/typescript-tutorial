242c94f397994e2fc2bcae1ac2a2393dd735713e
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-06 13:13:13 +0800

    learn function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("test");
+let greet;
+greet = () => {
+    console.log("hello");
+};
+const add1 = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+const add2 = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+    // if a function doesn't return anythings, the default type is "void"
+};
+add1(5, 10);
+add2(5, 10, "20");
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+// result = "something else";
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,27 @@
-console.log("test");
+let greet: Function;
+
+greet = () => {
+    console.log("hello");
+}
+
+const add1 = (a: number, b: number, c?: number | string) => {
+    console.log(a + b);
+    console.log(c);
+}
+
+const add2 = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b);
+    console.log(c);
+    // if a function doesn't return anythings, the default type is "void"
+}
+
+add1(5, 10);
+add2(5, 10, "20");
+
+const minus = (a: number, b: number) => {
+    return a + b;
+}
+
+let result = minus(10, 7);
+// result = "something else";
+
