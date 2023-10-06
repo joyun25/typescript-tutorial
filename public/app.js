76f11ab47a6ea342d5345c4a4aee34938eee8154
Jo Yun <joyunhsiao@gmail.com>
2023-10-06 19:25:56 +0800

    learn tuples

--- public/app.js
+++ public/app.js
@@ -12,32 +12,23 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// ENUMS
-var ResouceType;
-(function (ResouceType) {
-    ResouceType[ResouceType["BOOK"] = 0] = "BOOK";
-    ResouceType[ResouceType["AUTHOR"] = 1] = "AUTHOR";
-    ResouceType[ResouceType["FILM"] = 2] = "FILM";
-    ResouceType[ResouceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResouceType[ResouceType["PERSON"] = 4] = "PERSON";
-})(ResouceType || (ResouceType = {}));
-const docOne = {
-    uid: 1,
-    resourceType: ResouceType.BOOK,
-    data: { title: 'name of the wind' }
-};
-const docTwo = {
-    uid: 10,
-    resourceType: ResouceType.PERSON,
-    data: { name: 'yoshi' }
-};
-console.log(docOne, docTwo);
+// tuples
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false; // error
+let student;
+student = ['chun-li', 223423];
--- src/app.ts
+++ src/app.ts
@@ -18,35 +18,28 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber]
+
     let doc: HasFormatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
 
     list.render(doc, type.value, 'end');
 })
 
-// ENUMS
-enum ResouceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+// tuples
 
-interface Resource<T> {
-  uid: number;
-  resourceType: ResouceType;
-  data: T;
-}
+let arr = ['ryu', 25 , true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceType: ResouceType.BOOK, // vscode shows options when coding
-  data: { title: 'name of the wind' }
-}
+let tup: [string, number, boolean] = ['ryu', 25 , true];
+// tup[0] = false; // error
 
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceType: ResouceType.PERSON, // vscode shows options when coding
-  data: { name: 'yoshi' }
-}
-
-console.log(docOne, docTwo);
+let student: [string, number];
+student = ['chun-li', 223423];
