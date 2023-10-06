04b2fa4300c64df8505b528d0d4e514162b9ee64
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-06 15:18:29 +0800

    learn interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,22 +1,13 @@
-const me = {
-    name: 'shaun',
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent ', amount);
-        return amount;
-    },
-    // skills: [] // error
-};
-let someone;
-const greetPerson = (person) => {
-    console.log('hello ', person.name); // vscode will show methods here when using "person."
-};
-greetPerson(me);
-console.log(me);
-import { Invoice } from './classes/invoice.js';
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+let docOne;
+let docTwo;
+docOne = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing work', 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
@@ -33,5 +24,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,18 @@
-// interfaces
-interface IsPerson {
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
-}
+import { Invoice } from './classes/Invoice.js'
+import { Payment } from './classes/Payment.js'
+import { HasFormatter } from './interfaces/HasFormatter.js'
 
-const me: IsPerson = {
-    name: 'shaun',
-    age: 30,
-    speak(text: string): void {
-        console.log(text);
-    },
-    spend(amount: number): number {
-        console.log('I spent ', amount);
-        return amount
-    },
-    // skills: [] // error
-};
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
 
-let someone: IsPerson;
+docOne = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing work', 200);
 
-const greetPerson = (person: IsPerson) => {
-    console.log('hello ', person.name); // vscode will show methods here when using "person."
-}
+let docs: HasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
 
-greetPerson(me);
-
-console.log(me);
-
-import { Invoice } from './classes/invoice.js'
+console.log(docs);
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -53,10 +36,12 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+    let doc: HasFormatter;
+    if (type.value === 'invoice'){
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    console.log(doc);
 })
