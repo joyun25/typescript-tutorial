8d6a1cbc4da404875db0f282ab7778b20e0620bf
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-06 13:28:46 +0800

    learn function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,26 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+// let greet: Function;
+// example 1
+let greet; // "a" and "b" can be changed
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+// greet = (name: string, greeting: number) => {
+//     console.log(`${name} says ${greeting}`);
+// }
+// example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+    // it must return a number, so we have to add "else"
+};
+// example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
 };
